ed69fb1c0607a2e88d3b333c5c37956afed6d0f9
Sam Hartsfield <samh.public@gmail.com>
2022-06-25 14:46:30 -0400

    Add NixOS configuration for laptop
    First try at using NixOS!

---
+++ configuration.nix
@@ -0,0 +1,193 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.hostName = "nixos-xps"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  networking.networkmanager.enable = true;
+  # Manual says applet is needed for XFCE
+  programs.nm-applet.enable = true;
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp9s0.useDHCP = true;
+  #networking.interfaces.wlp11s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Plasma 5 Desktop Environment.
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.displayManager.defaultSession = "xfce";
+  services.xserver.desktopManager.xfce.thunarPlugins = [
+    pkgs.xfce.thunar-archive-plugin
+  ];
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.middleEmulation = true;
+  services.xserver.libinput.touchpad.scrollMethod = "edge";
+  services.xserver.libinput.touchpad.naturalScrolling = false;
+  services.xserver.libinput.touchpad.tapping = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+  users.users.samh = {
+    isNormalUser = true;
+    home = "/home/samh";
+    description = "Sam";
+    extraGroups = [ "wheel" "networkmanager" ];
+    #openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3Nza... alice@foobar" ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    bitwarden
+    element-desktop
+    firefox
+    htop
+    jetbrains.pycharm-professional
+    keepassxc
+    neofetch
+    obsidian
+    rclone
+    syncthing
+    tmux
+    tmuxPlugins.continuum
+    tmuxPlugins.resurrect
+    xfce.xfce4-whiskermenu-plugin
+    yadm
+  ];
+
+  programs.tmux.enable = true;
+
+  # TODO: only allow per package
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services = {
+    syncthing = {
+      enable = true;
+      user = "samh";
+      dataDir = "/home/samh/Documents";    # Default folder for new synced folders
+      configDir = "/home/samh/.config/syncthing";   # Folder for Syncthing's settings and keys
+
+      overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+      overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+      devices = {
+        "storage-server-2021" = { id = "AL433J4-2HM6N7D-C4HP5FT-6FNPCPI-MYW4T36-7RIEF5B-7J66U2W-BYW7CQ3"; };
+        "fedora2020desktop" = { id = "4JP4C67-VSQX646-E4BRJDC-ZQ2ZTNJ-CKWNUSS-2FC46OK-MDN7DB7-JCKBXQW"; };
+      };
+      folders = {
+        "Sync-Linux" = {        # Name of folder in Syncthing, also the folder ID
+          id = "Sync-Linux";
+          enable = true;
+          path = "/home/samh/Sync";    # Which folder to add to Syncthing
+          devices = [ "fedora2020desktop" ];      # Which devices to share the folder with
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "365";  # Syncthing doc says days; is Nix version the same?
+#              versionsPath = ".stversions";
+            };
+          };
+        };
+        "Notes-Shared" = {
+          id = "evgke-fvs53";
+          enable = true;
+          path = "/home/samh/Notes/Notes-Shared";
+          devices = [ "fedora2020desktop" ];
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "365";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
+
