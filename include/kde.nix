0ff4584c9b79fa6c6ce1b4ab319b3fc1dba589e8
Sam Hartsfield <samh.public@gmail.com>
2022-12-28 13:34:05 -0500

    Add kate/kwrite
    Remove kwallet stuff; it wasn't working.

--- include/kde.nix
+++ include/kde.nix
@@ -7,7 +7,8 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   environment.systemPackages = with pkgs; [
-    ark
+    ark # archive manager
+    kate # includes kwrite
   ];
 
   programs.partition-manager.enable = true; # KDE Partition Manager
@@ -15,16 +16,16 @@
   # Exclude some KDE Plasma packages
   # https://nixos.wiki/wiki/KDE
   # Attempt to disable kwallet to be able to use KeePassXC's secret service
-  services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
-    kwallet
-    kwallet-pam
-    kwalletmanager
-  ];
+  #services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
+  #  kwallet
+  #  kwallet-pam
+  #  kwalletmanager
+  #];
   # Try to override options from
   # https://github.com/NixOS/nixpkgs/blob/nixos-22.11/nixos/modules/services/x11/desktop-managers/plasma5.nix
   # to disable kwallet.
-  security.pam.services.gdm.enableKwallet = pkgs.lib.mkForce false;
-  security.pam.services.kdm.enableKwallet = pkgs.lib.mkForce false;
-  security.pam.services.lightdm.enableKwallet = pkgs.lib.mkForce false;
-  security.pam.services.sddm.enableKwallet = pkgs.lib.mkForce false;
+  #security.pam.services.gdm.enableKwallet = pkgs.lib.mkForce false;
+  #security.pam.services.kdm.enableKwallet = pkgs.lib.mkForce false;
+  #security.pam.services.lightdm.enableKwallet = pkgs.lib.mkForce false;
+  #security.pam.services.sddm.enableKwallet = pkgs.lib.mkForce false;
 }
