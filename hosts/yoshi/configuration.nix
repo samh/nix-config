d5e98398c14d154ee825e4f3f47e5a24ee949451
Sam Hartsfield <samh.public@gmail.com>
2023-09-18 16:45:47 -0400

    Move NVIDIA driver to separate file

--- hosts/yoshi/configuration.nix
+++ hosts/yoshi/configuration.nix
@@ -11,6 +11,7 @@
       ../include/ext-mounts.nix
       ../include/xfce.nix
       #../include/virt-manager.nix
+      ./nvidia-660ti.nix
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
@@ -31,38 +32,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # NVIDIA - 660 Ti supported by legacy 470 driver.
-  # https://nixos.wiki/wiki/Nvidia
-  # https://www.nvidia.com/en-us/drivers/unix/legacy-gpu/
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-  };
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "nvidia-x11"
-    "nvidia-settings"
-    "nvidia-persistenced"
-  ];
-
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
---
+++ hosts/yoshi/nvidia-660ti.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+{
+  # NVIDIA - 660 Ti supported by legacy 470 driver.
+  # https://nixos.wiki/wiki/Nvidia
+  # https://www.nvidia.com/en-us/drivers/unix/legacy-gpu/
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  };
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "nvidia-x11"
+    "nvidia-settings"
+    "nvidia-persistenced"
+  ];
+}
