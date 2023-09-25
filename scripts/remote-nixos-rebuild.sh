4fe4be8cb72321f588b7316f47b286bd03c6f8bf
Sam Hartsfield <samh.public@gmail.com>
2023-09-25 14:15:39 -0400

    Check arguments

--- scripts/remote-nixos-rebuild.sh
+++ scripts/remote-nixos-rebuild.sh
@@ -8,6 +8,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Error if TARGET is not set.
 : "${TARGET:?TARGET must be set}"
 
+# Check for at least one argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <nixos-rebuild arguments>"
+    echo
+    echo "Example: ./nixos-rebuild.sh switch"
+    exit 1
+fi
+
 #nixos-rebuild --flake .#${FLAKE_NAME} --target-host $TARGET --build-host $TARGET --use-remote-sudo "$@"
 
 set -x
