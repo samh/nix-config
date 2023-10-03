b040047ced1c791e0bd75118d062405f9eb6cf16
Sam Hartsfield <samh.public@gmail.com>
2023-10-02 23:57:45 -0400

    Add Jellyfin (not yet enabled)

--- hosts/yoshi/configuration.nix
+++ hosts/yoshi/configuration.nix
@@ -64,6 +64,22 @@
 
   # List services that you want to enable:
 
+  services.jellyfin.enable = false;
+  # https://jellyfin.org/docs/general/networking/index.html
+  networking.firewall = mkIf config.services.jellyfin.enable {
+    # 8096/tcp is used by default for HTTP traffic. You can change this in the dashboard.
+    # 8920/tcp is used by default for HTTPS traffic. You can change this in the dashboard.
+    allowedTCPPorts = [8096 8920];
+    # 1900/udp is used for service auto-discovery. This is not configurable.
+    # (DLNA also uses this port and is required to be in the local subnet.)
+    #
+    # 7359/udp is also used for auto-discovery. This is not configurable.
+    # Allows clients to discover Jellyfin on the local network. A broadcast
+    # message to this port with Who is JellyfinServer? will get a JSON
+    # response that includes the server address, ID, and name.
+    allowedUDPPorts = [1900 7359];
+  };
+
   #virtualisation.oci-containers.backend = "podman";
 
   # Open ports in the firewall.
