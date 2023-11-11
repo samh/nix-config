c7a1578d6283b6b8f0fc5c7c931ce2e36850bb6b
Sam Hartsfield <samh.public@gmail.com>
2023-11-10 23:29:37 -0500

    Add nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.05";
@@ -14,11 +18,10 @@
 
     hardware.url = "github:nixos/nixos-hardware";
 
-    nix-config-shared.url = "github:samh/nix-config-shared";
-    nix-config-shared.inputs.nixpkgs.follows = "nixpkgs";
-    nix-config-shared.inputs.home-manager.follows = "home-manager";
-    # Make sure unstable also follows, if we add it as a direct input.
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    samh-shared.url = "github:samh/nix-config-shared";
+    samh-shared.inputs.nixpkgs.follows = "nixpkgs";
+    samh-shared.inputs.home-manager.follows = "home-manager";
+    samh-shared.inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
 
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
