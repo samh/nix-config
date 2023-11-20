e0891a12d62d73faa8104ba7bcc9ede6900dee27
Sam Hartsfield <samh.public@gmail.com>
2023-11-20 17:04:19 -0500

    Add option to sync only to main script

--- scripts/remote-nixos-rebuild.sh
+++ scripts/remote-nixos-rebuild.sh
@@ -13,7 +13,17 @@ if [ $# -eq 0 ]; then
     echo "Usage: $0 <nixos-rebuild arguments>"
     echo
     echo "Example: ./nixos-rebuild.sh switch"
-    exit 1
+    echo
+    # Prompt y/n if the user wants to just sync
+    read -p "Would you like to run sync? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        "$DIR"/sync.sh
+        exit $?
+    else
+        echo "Exiting..."
+        exit 1
+    fi
 fi
 
 #nixos-rebuild --flake .#${FLAKE_NAME} --target-host $TARGET --build-host $TARGET --use-remote-sudo "$@"
