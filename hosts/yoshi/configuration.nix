7ccc09c46d2387253eb5876f3f8b6046de3a44f5
Sam Hartsfield <samh.public@gmail.com>
2023-12-03 18:20:50 -0500

    Reformat grub to be like my other configs

--- hosts/yoshi/configuration.nix
+++ hosts/yoshi/configuration.nix
@@ -27,11 +27,13 @@
 
   # Use the grub boot loader (EFI mode)
   boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.configurationLimit = 25;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+    configurationLimit = 25;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Load the kernel module for the Blu-Ray drive.
