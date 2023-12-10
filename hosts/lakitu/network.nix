673b317a67b91efdedbaaf82ef749afa86ccf528
Sam Hartsfield <samh.public@gmail.com>
2023-12-10 11:10:08 -0500

    Set network interface names

--- hosts/lakitu/network.nix
+++ hosts/lakitu/network.nix
@@ -1,3 +1,8 @@
+# NICs:
+# eno1 - built-in 1Gb
+# enp4s0 - 2.5Gb left/bottom port
+# enp5s0 - 2.5Gb right/top port
+#
 {config, ...}: {
   # https://nixos.wiki/wiki/Systemd-networkd
   # https://www.freedesktop.org/software/systemd/man/latest/systemd-networkd.service.html
@@ -22,17 +27,18 @@
     # See https://www.freedesktop.org/software/systemd/man/latest/systemd.network.html
     networks = {
       # Connect the bridge ports to the bridge
-      "30-enp1s0" = {
-        matchConfig.Name = "enp2s0"; # top NIC
+      "30-enp4s0" = {
+        matchConfig.Name = "enp4s0"; # left port
         networkConfig.Bridge = "br0";
         linkConfig.RequiredForOnline = "enslaved";
       };
-      "30-enp2s0" = {
-        matchConfig.Name = "enp5s4"; # bottom NIC
+      "30-enp5s0" = {
+        matchConfig.Name = "enp5s0"; # right port
         networkConfig.Bridge = "br1";
         linkConfig.RequiredForOnline = "enslaved";
       };
       # Configure the bridge networks
+      # br0 is the WAN side. Host should not have an IP.
       "40-br0" = {
         matchConfig.Name = "br0";
         bridgeConfig = {};
@@ -41,6 +47,8 @@
           ActivationPolicy = "down";
         };
       };
+      # br1 is the LAN side.
+      # Configure it so the host can get an IP address from the router VM.
       "40-br1" = {
         matchConfig.Name = "br1";
         networkConfig.DHCP = "ipv4";
