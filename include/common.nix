89f1b638c9189b3421e99e68444e6c264a87bbb0
Sam Hartsfield <samh.public@gmail.com>
2023-12-12 12:21:07 -0500

    Add i18n settings
    Copied from configuration.nix created by the NixOS installer.

--- include/common.nix
+++ include/common.nix
@@ -17,6 +17,21 @@
 
   time.timeZone = lib.mkDefault "America/New_York";
 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
   # Define a group for access to multimedia files, e.g. videos, music.
   users.groups.multimedia = {
     gid = 20050;
