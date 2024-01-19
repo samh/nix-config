fd484273f2ad30226af6c534a93bd292a4699b85
Sam Hartsfield <samh.public@gmail.com>
2024-01-19 15:26:45 -0500

    Add samba mounts on desktop

--- hosts/desktop-z390-9700k/mounts.nix
+++ hosts/desktop-z390-9700k/mounts.nix
@@ -2,7 +2,23 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  # this line prevents hanging on network split
+  net_automount_opts = [
+    "x-systemd.after=network-online.target"
+    "x-systemd.automount"
+    "noauto"
+    "_netdev"
+    "x-systemd.idle-timeout=60"
+    "x-systemd.device-timeout=5s"
+    "x-systemd.mount-timeout=5s"
+  ];
+  samba_permission_opts = [
+    "uid=${toString config.users.users.${config.my.user}.uid}"
+    "file_mode=0660"
+    "dir_mode=0770"
+  ];
+in {
   # Scrub for pools is in common.nix. This adds to that list.
   services.btrfs.autoScrub.fileSystems = [
     "/var/lib/flatpak"
@@ -88,6 +104,36 @@
     fsType = "ext4";
     options = ["nofail" "noatime" "x-systemd.device-timeout=0"];
   };
+
+  # Remote filesystems
+  # https://nixos.wiki/wiki/Samba
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [pkgs.cifs-utils];
+  fileSystems."/mnt/storage" = {
+    device = "//yoshi.hartsfield.xyz/storage";
+    fsType = "cifs";
+    options =
+      net_automount_opts
+      ++ samba_permission_opts
+      ++ [
+        "credentials=/root/smb-secrets"
+      ];
+  };
+  fileSystems."/mnt/Library" = {
+    device = "//yoshi.hartsfield.xyz/Library";
+    fsType = "cifs";
+    options =
+      net_automount_opts
+      ++ samba_permission_opts
+      ++ [
+        "credentials=/root/smb-secrets"
+        "gid=${toString config.users.groups.multimedia.gid}"
+      ];
+  };
+  # Add tmpfiles rule to create symbolic link from /storage to /mnt/storage
+  systemd.tmpfiles.rules = [
+    "L /storage - - - - /mnt/storage"
+  ];
 
   # Root pools for btrbk backups and general management.
   # Also, /pool mounts are automatically enabled for autoScrub in common.nix.
