92a94f38a58fc304bbe2b30f2ed32ec5f1680c05
Sam Hartsfield <samh.public@gmail.com>
2024-06-09 22:46:30 -0400

    Update renamed options

--- hosts/bowser/configuration.nix
+++ hosts/bowser/configuration.nix
@@ -55,13 +55,13 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "samh";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "samh";
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
@@ -96,5 +96,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
