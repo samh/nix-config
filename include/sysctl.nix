6ba1d30d7bf5a9d7f8b85b870cb8fd9529c04041
Sam Hartsfield <samh.public@gmail.com>
2024-07-20 10:21:06 -0400

    Add sysctl settings for Syncthing optimization

--- include/sysctl.nix
+++ include/sysctl.nix
@@ -4,7 +4,15 @@
   ...
 }: {
   config = {
-    # Enable sysrq keyboard shortcuts; see https://wiki.archlinux.org/title/Keyboard_shortcuts#Kernel_(SysRq)
-    boot.kernel.sysctl."kernel.sysrq" = lib.mkDefault 1;
+    boot.kernel.sysctl = {
+      # Enable sysrq keyboard shortcuts; see
+      # https://wiki.archlinux.org/title/Keyboard_shortcuts#Kernel_(SysRq)
+      "kernel.sysrq" = lib.mkDefault 1;
+      # Increase UDP max buffer size to allow for faster QUIC transfers
+      # on high-bandwidth connections in Syncthing.
+      # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
+      "net.core.rmem_max" = lib.mkDefault 7500000;
+      "net.core.wmem_max" = lib.mkDefault 7500000;
+    };
   };
 }
