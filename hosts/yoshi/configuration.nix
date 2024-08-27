7af59537294e065d59e546d528af1fe74293f0d2
Sam Hartsfield <samh.public@gmail.com>
2024-08-27 12:13:48 -0400

    yoshi: makemkv

--- hosts/yoshi/configuration.nix
+++ hosts/yoshi/configuration.nix
@@ -110,6 +110,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     yt-dlp
+    pkgs.unstable.makemkv
   ];
 
   # List services that you want to enable:
