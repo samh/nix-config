560e1ef6042ba87284b576bc621c9c8254d7d08d
Sam Hartsfield <samh.public@gmail.com>
2025-01-29 10:59:50 -0500

    bowser: add drive for AI data

--- hosts/bowser/mounts.nix
+++ hosts/bowser/mounts.nix
@@ -4,6 +4,8 @@
   ...
 }: let
   btrfs_opts = ["compress=zstd:9"];
+  # Most of this data will not be compressible.
+  btrfs_opts_ai = ["compress=zstd:1"];
 in {
   fileSystems."/home" = {
     device = "LABEL=gaming-home";
@@ -19,6 +21,16 @@ in {
     device = "LABEL=game-2";
     fsType = "btrfs";
     options = ["subvol=@games"] ++ btrfs_opts;
+  };
+  fileSystems."/var/lib/private/ollama" = {
+    device = "LABEL=ai-data";
+    fsType = "btrfs";
+    options = ["subvol=@ollama"] ++ btrfs_opts_ai;
+  };
+  fileSystems."/var/lib/private/open-webui" = {
+    device = "LABEL=ai-data";
+    fsType = "btrfs";
+    options = ["subvol=@open-webui"] ++ btrfs_opts_ai;
   };
 
   # Root pools for btrbk backups and general management.
@@ -37,5 +49,10 @@ in {
     device = "LABEL=game-2";
     fsType = "btrfs";
     options = btrfs_opts;
+  };
+  fileSystems."/pool/ai" = {
+    device = "LABEL=ai-data";
+    fsType = "btrfs";
+    options = btrfs_opts_ai;
   };
 }
