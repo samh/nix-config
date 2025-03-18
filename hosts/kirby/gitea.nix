d229f998d7817579ae95272cc80712828f421f65
Sam Hartsfield <samh.public@gmail.com>
2025-03-18 12:06:18 -0400

    kirby: Adjust gitea login remember days

--- hosts/kirby/gitea.nix
+++ hosts/kirby/gitea.nix
@@ -33,6 +33,9 @@ in {
       };
       # You can temporarily allow registration to create an admin user.
       service.DISABLE_REGISTRATION = true;
+      security = {
+        LOGIN_REMEMBER_DAYS = 90; # default is 31
+      };
       # Add support for actions, based on act: https://github.com/nektos/act
       actions = {
         ENABLED = true;
